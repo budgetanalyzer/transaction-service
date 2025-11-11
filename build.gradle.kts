b5eff12d745b63737e27f9e4620e56b30236b806
Bleu Rubin <bleurubin@gmail.com>
2025-11-11 15:06:42 -0500

    upgrade to spring boot 3.5.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     checkstyle
-    id("org.springframework.boot") version "3.5.6"
+    id("org.springframework.boot") version "3.5.7"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.diffplug.spotless") version "8.0.0"
 }
