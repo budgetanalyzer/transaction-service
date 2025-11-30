11b5e397882dcc0124226e3faa9a618d751ea314
Bleu Rubin <bleurubin@gmail.com>
2025-11-30 06:52:37 -0500

    formatting

--- src/main/java/org/budgetanalyzer/transaction/api/request/TransactionFilter.java
+++ src/main/java/org/budgetanalyzer/transaction/api/request/TransactionFilter.java
@@ -18,9 +18,7 @@ public record TransactionFilter(
             description = "Identifier for the account associated with the transaction",
             example = "checking-3223")
         String accountId,
-    @Schema(
-            description = "Name of the bank where the transaction occurred",
-            example = "USD Bank")
+    @Schema(description = "Name of the bank where the transaction occurred", example = "USD Bank")
         String bankName,
     @Schema(description = "Start date for transaction date range", example = "2025-10-01")
         LocalDate dateFrom,
